00cda9bbbb838444de8abb7ca5e5778d6a4d0bb3
conniely <conniely04@gmail.com>
2024-01-28 23:04:03 -0800

    Initial Commit

---
+++ StudySync/StudySyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySyncApp.swift
+//  StudySync
+//
+//  Created by Connie Ly on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct StudySyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
